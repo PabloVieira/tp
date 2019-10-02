c70a10e0b4a578d07960c2fd47a73a8ea742a5d4
PabloVieira <pablo.vieira@acad.pucrs.br>
2019-10-02 18:45:08 -0300

    inst_branch, inst_grupo1

--- tp2/datapath.vhd
+++ tp2/datapath.vhd
@@ -21,21 +21,30 @@ architecture datapath of datapath is
     signal incpc, pc, npc1, npc2, IR, result, R1, R2, RA, RB, RIN, ext16, cte_im, IMED, op1, op2, 
            outalu, RALU, MDR, mdr_int, dtpc : std_logic_vector(31 downto 0) := (others=> '0');
     signal adD, adS : std_logic_vector(4 downto 0) := (others=> '0');    
-    signal inst_branch, inst_grupo1, inst_grupoI: std_logic;   
+    signal inst_branch2e, inst_branch3e, inst_branch5e, inst_grupo1e3, inst_grupo1e5: std_logic;   
     signal salta : std_logic := '0';
-    signal controlSignals1, controlSignals2, controlSignals3, controlSignals4: sinalDeControle;
+    signal controlSignals2e, controlSignals3e, controlSignals4e, controlSignals5e: sinalDeControle;
 begin
 
    -- auxiliary signals 
-   inst_branch  <= '1' when uins.i=BEQ or uins.i=BGEZ or uins.i=BLEZ or uins.i=BNE else 
-                  '0';
+   inst_branch2e  <= '1' when controlSignals2e.ULAOp=BEQ or controlSignals2e.ULAOp=BGEZ or
+                             controlSignals2e.ULAOp=BLEZ or controlSignals2e.ULAOp=BNE
+                        else '0';
+   inst_branch3e  <= '1' when controlSignals3e.ULAOp=BEQ or controlSignals3e.ULAOp=BGEZ or
+                             controlSignals3e.ULAOp=BLEZ or controlSignals3e.ULAOp=BNE
+                        else '0';
+   inst_branch5e  <= '1' when controlSignals4e.ULAOp=BEQ or controlSignals4e.ULAOp=BGEZ or
+                             controlSignals4e.ULAOp=BLEZ or controlSignals4e.ULAOp=BNE
+                        else '0';
                   
-   inst_grupo1  <= '1' when uins.i=ADDU or uins.i=NOP or uins.i=SUBU or uins.i=AAND
-                         or uins.i=OOR or uins.i=XXOR or uins.i=NNOR else
-                   '0';
-
-   inst_grupoI  <= '1' when uins.i=ADDIU or uins.i=ANDI or uins.i=ORI or uins.i=XORI else
-                   '0';
+   inst_grupo1e3  <= '1' when controlSignals3e.ULAOp=ADDU or controlSignals3e.ULAOp=NOP or controlSignals3e.ULAOp=SUBU or
+                              controlSignals3e.ULAOp=AAND or controlSignals3e.ULAOp=OOR or controlSignals3e.ULAOp=XXOR or
+                              controlSignals3e.ULAOp=NNOR
+                         else '0';
+   inst_grupo1e5  <= '1' when controlSignals5e.ULAOp=ADDU or controlSignals5e.ULAOp=NOP or controlSignals5e.ULAOp=SUBU or
+                              controlSignals5e.ULAOp=AAND or controlSignals5e.ULAOp=OOR or controlSignals5e.ULAOp=XXOR or
+                              controlSignals5e.ULAOp=NNOR
+                        else '0';                
 
    --==============================================================================
    -- first_stage
@@ -52,27 +61,26 @@ begin
    --==============================================================================
    -- second stage
    --==============================================================================
-   ct: entity work.control_unit
-            port map(ir=>IR, sinaisDeControle=>controlSignals1);
+   ct: entity work.control_unit  port map(ir=>IR, sinaisDeControle=>controlSignals2e);
                 
    -- The then clause is only used for logic shifts with shamt field       
-   adS <= IR(20 downto 16) when controlSignals1.ULAOp=SSLL or uins.i=SSRA or uins.i=SSRL else 
-          IR(25 downto 21);
+   adS <= IR(20 downto 16) when controlSignals2e.ULAOp=SSLL or controlSignals2e.ULAOp=SSRA or controlSignals2e.ULAOp=SSRL
+                           else IR(25 downto 21);
           
    REGS: entity work.reg_bank(reg_bank) port map
         (ck=>ck, rst=>rst, wreg=>uins.wreg, AdRs=>adS, AdRt=>ir(20 downto 16), adRD=>adD,  
          Rd=>RIN, R1=>R1, R2=>R2);
     
    -- sign extension 
-   ext16 <=  x"FFFF" & IR(15 downto 0) when IR(15)='1' else
-             x"0000" & IR(15 downto 0);
+   ext16 <=  x"FFFF" & IR(15 downto 0) when IR(15)='1'
+                                       else x"0000" & IR(15 downto 0);
     
    -- Immediate constant
-   cte_im <= ext16(29 downto 0)  & "00"     when inst_branch='1'     else
+   cte_im <= ext16(29 downto 0)  & "00"     when inst_branch2e='1'     else
                 -- branch address adjustment for word frontier
-             "0000" & IR(25 downto 0) & "00" when uins.i=J or uins.i=JAL else
+             "0000" & IR(25 downto 0) & "00" when controlSignals2e.ULAOp=J or controlSignals2e.ULAOp=JAL else
                 -- J/JAL are word addressed. MSB four bits are defined at the ALU, not here!
-             x"0000" & IR(15 downto 0) when uins.i=ANDI or uins.i=ORI  or uins.i=XORI else
+             x"0000" & IR(15 downto 0) when controlSignals2e.ULAOp=ANDI or controlSignals2e.ULAOp=ORI  or controlSignals2e.ULAOp=XORI else
                 -- logic instructions with immediate operand are zero extended
              ext16;
                 -- The default case is used by addiu, lbu, lw, sbu and sw instructions
@@ -87,8 +95,8 @@ begin
       IMED => IMED,
       npcIN => npc1,
       npcOUT => npc2,
-      controlSignalsIN => controlSignals1,
-      controlSignalsOUT => controlSignals2
+      controlSignalsIN => controlSignals2e,
+      controlSignalsOUT => controlSignals3e
    );
  
   --==============================================================================
@@ -96,31 +104,28 @@ begin
    --==============================================================================
                       
    -- select the first ALU operand                           
-   op1 <= npc  when inst_branch='1' else 
+   op1 <= npc  when inst_branch3e='1' else 
           RA; 
      
    -- select the second ALU operand
-   op2 <= RB when inst_grupo1='1' or controlSignals2.i=SLTU or controlSignals2.i=SLT or controlSignals2.i=JR 
-                  or controlSignals2.i=SLLV or controlSignals2.i=SRAV or controlSignals2.i=SRLV else 
-          IMED; 
+   op2 <= RB when inst_grupo1='1' or controlSignals3e.ULAOp=SLTU or controlSignals3e.ULAOp=SLT or controlSignals3e.ULAOp=JR 
+                  or controlSignals3e.ULAOp=SLLV or controlSignals3e.ULAOp=SRAV or controlSignals3e.ULAOp=SRLV
+                  else IMED; 
                  
    -- ALU instantiation
-   inst_alu: entity work.alu port map (op1=>op1, op2=>op2, outalu=>outalu, op_alu=>uins.i);
-                                   
-   -- ALU register
-   --REG_alu: entity work.regnbit  port map(ck=>ck, rst=>rst, ce=>uins.walu, D=>outalu, Q=>RALU);               
+   inst_alu: entity work.alu port map (op1=>op1, op2=>op2, outalu=>outalu, op_alu=>controlSignals3e.ULAOp);
  
    -- evaluation of conditions to take the branch instructions
-   salta <=  '1' when ( (RA=RB  and uins.i=BEQ)  or (RA>=0  and uins.i=BGEZ) or
-                        (RA<=0  and uins.i=BLEZ) or (RA/=RB and uins.i=BNE) )  else
+   salta <=  '1' when ( (RA=RB  and controlSignals3e.ULAOp=BEQ)  or (RA>=0  and controlSignals3e.ULAOp=BGEZ) or
+                        (RA<=0  and controlSignals3e.ULAOp=BLEZ) or (RA/=RB and controlSignals3e.ULAOp=BNE) )  else
              '0';
                  
    EXMEM: entity work.exmem port map (
       ck => ck,
       outalu => outalu,
       RALU => RALU,
-      controlSignalsIN => controlSignals2,
-      controlSignalsOUT => controlSignals3
+      controlSignalsIN => controlSignals3e,
+      controlSignalsOUT => controlSignals4e
    );
              
    --==============================================================================
@@ -133,20 +138,18 @@ begin
    data <= RB when (uins.ce='1' and uins.rw='0') else (others=>'Z');  
 
    -- single byte reading from memory  -- SUPONDO LITTLE ENDIAN
-   mdr_int <= data when uins.i=LW  else
-              x"000000" & data(7 downto 0);
+   mdr_int <= data when controlSignals4e.ULAOp=LW  else x"000000" & data(7 downto 0);
        
    --RMDR: entity work.regnbit  port map(ck=>ck, rst=>rst, ce=>uins.wmdr, D=>mdr_int, Q=>MDR);                 
   
-   result <=    MDR when uins.i=LW  or uins.i=LBU else
-                RALU;
+   result <= MDR when controlSignals4e.ULAOp=LW  or controlSignals4e.ULAOp=LBU else RALU;
 
    MEMER: entity work.memer port map (
       ck => ck,
       mdr_int => mdr_int,
       MDR => MDR,
-      controlSignalsIN => controlSignals3,
-      controlSignalsOUT => controlSignals4
+      controlSignalsIN => controlSignals4e,
+      controlSignalsOUT => controlSignals5e
    );  
 
    --==============================================================================
@@ -154,27 +157,25 @@ begin
    --==============================================================================
 
    -- signal to be written into the register bank
-   RIN <= npc when (uins.i=JALR or uins.i=JAL) else result;
+   RIN <= npc when (controlSignals5e.ULAOp=JALR or controlSignals5e.ULAOp=JAL) else result;
    
    -- register bank write address selection
-   adD <= "11111"               when uins.i=JAL else -- JAL writes in register $31
-         IR(15 downto 11)       when inst_grupo1='1' or uins.i=SLTU or uins.i=SLT
-                                                     or uins.i=JALR  
-						     or uins.i=SSLL or uins.i=SLLV
-						     or uins.i=SSRA or uins.i=SRAV
-						     or uins.i=SSRL or uins.i=SRLV
+   adD <= "11111"               when controlSignals5e.ULAOp=JAL else -- JAL writes in register $31
+         IR(15 downto 11)       when inst_grupo1='1' or controlSignals5e.ULAOp=SLTU or controlSignals5e.ULAOp=SLT
+                                                     or controlSignals5e.ULAOp=JALR  
+						     or controlSignals5e.ULAOp=SSLL or controlSignals5e.ULAOp=SLLV
+						     or controlSignals5e.ULAOp=SSRA or controlSignals5e.ULAOp=SRAV
+						     or controlSignals5e.ULAOp=SSRL or controlSignals5e.ULAOp=SRLV
                                                      else
-         IR(20 downto 16) -- inst_grupoI='1' or uins.i=SLTIU or uins.i=SLTI 
-        ;                 -- or uins.i=LW or  uins.i=LBU  or uins.i=LUI, or default
+         IR(20 downto 16) -- inst_grupoI='1' or uins.ULAOp=SLTIU or uins.ULAOp=SLTI 
+        ;                 -- or uins.ULAOp=LW or  uins.ULAOp=LBU  or uins.ULAOp=LUI, or default
     
-   dtpc <= result when (inst_branch='1' and salta='1') or uins.i=J    or uins.i=JAL or uins.i=JALR or uins.i=JR  
+   dtpc <= result when (inst_branch5e='1' and salta='1') or controlSignals5e.ULAOp=J    or controlSignals5e.ULAOp=JAL or controlSignals5e.ULAOp=JALR or controlSignals5e.ULAOp=JR  
            else npc;
    
    -- Code memory starting address: beware of the OFFSET! 
    -- The one below (x"00400000") serves for code generated 
    -- by the MARS simulator
-   --rpc: entity work.regnbit generic map(INIT_VALUE=>x"00400000")   
-     --                       port map(ck=>ck, rst=>rst, ce=>uins.wpc, D=>dtpc, Q=>pc);
 
    ERBI: entity work.erbi port map (
       ck => ck,
